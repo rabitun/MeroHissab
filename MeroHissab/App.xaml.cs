22863dcaa34f9cc332fde373a957792dcd8b838f
rabitun <rajina.gurung.s23@icp.edu.np>
2024-12-25 09:10:36 +0545

    Add project files.

---
+++ MeroHissab/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MeroHissab
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
